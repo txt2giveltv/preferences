c7ec0ab337612e22a0b3dbd782a3623efa034933
Elvis Ozoria <elvis.ozoria@gmail.com>
2022-08-18 10:38:56 -0400

    adds explicit sql type metadata

--- lib/preferences/preference_definition.rb
+++ lib/preferences/preference_definition.rb
@@ -1,47 +1,81 @@
 module Preferences
   # Represents the definition of a preference for a particular model
   class PreferenceDefinition
+    TYPES = {
+      boolean: ActiveRecord::Type::Boolean,
+      date: ActiveRecord::Type::Date,
+      datetime: ActiveRecord::Type::DateTime,
+      decimal: ActiveRecord::Type::Decimal,
+      float: ActiveRecord::Type::Float,
+      integer: ActiveRecord::Type::Integer,
+      string: ActiveRecord::Type::String
+    }
+
+    TYPES.default = ActiveRecord::Type::Value
+
     # The data type for the content stored in this preference type
     attr_reader :type
-    
+
     def initialize(name, *args) #:nodoc:
       options = args.extract_options!
       options.assert_valid_keys(:default, :group_defaults)
-      
       @type = args.first ? args.first.to_sym : :boolean
-      
+      cast_type = if @type == :any
+        nil
+      else
+        ActiveRecord::Type.const_get(@type.to_s.camelize).new
+      end
+      puts "### VAR YYYY cast_type  ###>>  #{cast_type.type}"
+      sql_type_metadata = ActiveRecord::ConnectionAdapters::SqlTypeMetadata.new(
+        sql_type: cast_type.type.to_s,
+        type: cast_type.type,
+        limit: cast_type.limit,
+        precision: cast_type.precision,
+        scale: cast_type.scale)
       # Create a column that will be responsible for typecasting
-      @column = ActiveRecord::ConnectionAdapters::Column.new(name.to_s, options[:default], @type == :any ? nil : @type.to_s)
-      
+      @column = ActiveRecord::ConnectionAdapters::Column.new(name.to_s, sql_type_metadata)
       @group_defaults = (options[:group_defaults] || {}).inject({}) do |defaults, (group, default)|
         defaults[group.is_a?(Symbol) ? group.to_s : group] = type_cast(default)
         defaults
       end
     end
-    
+
     # The name of the preference
     def name
       @column.name
     end
-    
+
     # The default value to use for the preference in case none have been
     # previously defined
     def default_value(group = nil)
       @group_defaults.include?(group) ? @group_defaults[group] : @column.default
     end
-    
+
     # Determines whether column backing this preference stores numberic values
     def number?
-      @column.number?
+      [:integer, :float].include?(@column.sql_type_metadata.type)
     end
-    
+
     # Typecasts the value based on the type of preference that was defined.
-    # This uses ActiveRecord's typecast functionality so the same rules for
-    # typecasting a model's columns apply here.
+    # This uses functionality added in to ActiveRecord's attributes api in Rails 5
+    # so the same rules for typecasting a model's columns apply here.
     def type_cast(value)
-      @type == :any ? value : @column.type_cast(value)
+      @type == :any ? value : cast(@column.type, value)
     end
-    
+
+    def cast(type, value)
+      return nil if value.nil?
+
+      case type
+      when :string, :decimal
+        value
+      when :integer, :float, :datetime, :date, :boolean
+        TYPES[type].new.cast(value)
+      else value
+        # Nothing.
+      end
+    end
+
     # Typecasts the value to true/false depending on the type of preference
     def query(value)
       if !(value = type_cast(value))
